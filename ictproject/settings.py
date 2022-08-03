919114998c0496f071e4fc14d0feb9f8955e62b2
Jakom001 <56824003+Jakom001@users.noreply.github.com>
2022-08-03 16:52:07 +0300

    'database'

--- ictproject/settings.py
+++ ictproject/settings.py
@@ -184,6 +184,10 @@ MESSAGE_TAGS = {
 AUTH_EXEMPT_ROUTES = ('register', 'login', 'password_reset', 'admin')
 AUTH_LOGIN_ROUTE = 'login'
 
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "login"
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
