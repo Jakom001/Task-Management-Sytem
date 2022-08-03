53edf6ab56545f41cd463994dfd574d8296c15a4
Jakom001 <56824003+Jakom001@users.noreply.github.com>
2022-08-03 16:35:54 +0300

    'database'

--- ictproject/settings.py
+++ ictproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-z(2u2^e=-zgr1c$4_67#0p4n^6666o2kn45_w2w#z@v4kj_kjh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
